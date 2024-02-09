18362fa5556021cad0e9c2fa54d1a264b5c95d6e
diogo <diogobrasildasilva@gmail.com>
2024-02-09 19:12:32 -0300

    Starting to implement Board and Piece.

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) throws Exception {
+        
         Position p = new Position(3, 5);
         System.out.println(p);
     }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,6 +1,7 @@
 package boardgame;
 
 public class Position {
+  
   private int row;
   private int column;
 
@@ -8,7 +9,7 @@ public class Position {
     this.row = row;
     this.column = column;
   }
-  
+
   public int getRow() {
     return row;
   }
